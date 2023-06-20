b45e5d1661cfcc5c4e52f6541dc18b5219d8750c
elenaeslin <srta.scobar@gmail.com>
2023-06-20 10:03:43 +0200

    Complete tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let strSplit = str.toLowerCase().split(/[., ,?,!]/).join("");
+    let strSplitReverse = strSplit.split("").reverse().join("");
 
+    // Compare both
+    return strSplit === strSplitReverse
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+//get array and erase spaces, commas and make it lowerCase
+// compare full string with reversed one
